b5ac4d213ea935b26abca64569ac37e5fb82f7ed
Mateus <Mateus@>
2020-08-22 20:07:44 -0300

    - Layout da Tela Principal

--- GrafosProjeto1/src/grafosprojeto1/TelaPrincipalController.java
+++ GrafosProjeto1/src/grafosprojeto1/TelaPrincipalController.java
@@ -1,27 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grafosprojeto1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-/**
- * FXML Controller class
- *
- * @author vicga
- */
 public class TelaPrincipalController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private VBox pnPrincipal;
+    @FXML
+    private Canvas pnGrafo;
+    @FXML
+    private HBox pnMatrizes;
+    @FXML
+    private Pane pnMatrizAdj;
+    @FXML
+    private Pane pnMatrizInc;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        
+    }      
 }
