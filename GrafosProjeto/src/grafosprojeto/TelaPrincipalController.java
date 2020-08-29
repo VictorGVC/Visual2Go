78523b29dd3c0a91d5c841230369575188510776
Mateus <Mateus@>
2020-08-29 17:03:15 -0300

    - Matriz de incidencia e adjacencia adicionando valores

--- GrafosProjeto/src/grafosprojeto/TelaPrincipalController.java
+++ GrafosProjeto/src/grafosprojeto/TelaPrincipalController.java
@@ -4,12 +4,15 @@ import grafosprojeto.objetos.Aresta;
 import grafosprojeto.objetos.Grafo;
 import grafosprojeto.objetos.Vertice;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
@@ -17,13 +20,12 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Circle;
 
 public class TelaPrincipalController implements Initializable {
 
     Grafo g;
     char cont;
-    int qtdeV = 0,conta;
+    int qtdeInc, conta;
     
     @FXML
     private VBox pnPrincipal;
@@ -41,18 +43,19 @@ public class TelaPrincipalController implements Initializable {
         cont = 64;
         inicializaGp();
         conta = 0;
+        qtdeInc = 1;
     }    
     
     private void inicializaGp(){
         
         gpmatrizadj.setMinSize(325, 185);
-        gpmatrizadj.setVgap(2); 
-        gpmatrizadj.setHgap(2);
+        //gpmatrizadj.setVgap(2); 
+        //gpmatrizadj.setHgap(2);
         gpmatrizadj.setAlignment(Pos.CENTER_RIGHT);
         
         gpmatrizinc.setMinSize(325, 185);
-        gpmatrizadj.setVgap(2); 
-        gpmatrizinc.setHgap(2);
+        //gpmatrizadj.setVgap(2); 
+        //gpmatrizinc.setHgap(2);
         gpmatrizinc.setAlignment(Pos.CENTER_LEFT);
     }
     
@@ -90,7 +93,7 @@ public class TelaPrincipalController implements Initializable {
                 {
                     String[] s = g.getStatus().split("/");
                     g.setStatus("");
-                    criaVertice(s[1].charAt(0),v.getID());
+                    criaAresta(s[1].charAt(0),v.getID());
                 }
             }
         };
@@ -98,9 +101,10 @@ public class TelaPrincipalController implements Initializable {
         v.getAp().addEventFilter(MouseEvent.MOUSE_CLICKED, clicked);
     }
     
-    private void criaVertice(char id1, char id2)
+    private void criaAresta(char id1, char id2)
     {
-        Vertice v1 = null,v2 = null;
+        Vertice v1 = null, v2 = null;
+        
         for (Vertice v : g.getVlist()) 
         {
             if(v.getID() == id1)
@@ -109,14 +113,47 @@ public class TelaPrincipalController implements Initializable {
                 v2 = v;
         }
         Aresta a = new Aresta(conta++,v1,v2,'g',pngrafo);
+        atualizaMatriz(a);
+    }
+    
+    private void atualizaMatriz(Aresta a){
+        
+        Node n = null;
+        
+        for (Node node : gpmatrizadj.getChildren()) {
+            if(node instanceof Label && gpmatrizadj.getRowIndex(node) == a.getID1()-64 && gpmatrizadj.getColumnIndex(node) == a.getID2()-64)
+                n = node;
+        }
+        gpmatrizadj.getChildren().remove(n);
+        for (Node node : gpmatrizadj.getChildren()) {
+            if(node instanceof Label && gpmatrizadj.getRowIndex(node) == a.getID2()-64 && gpmatrizadj.getColumnIndex(node) == a.getID1()-64)
+                n = node;
+        }
+        gpmatrizadj.getChildren().remove(n);
+        
+        gpmatrizadj.add(new Label(""+1),a.getID1()-64,a.getID2()-64);
+        gpmatrizadj.add(new Label(""+1),a.getID2()-64,a.getID1()-64);
+        
+        for (int i = 1; i <= g.getVlist().size(); i++)
+            if(i != a.getID1()-64 && i != a.getID2()-64 )
+                gpmatrizinc.add(new Label(""+0),a.getID1()-64,i);
+                
+        gpmatrizinc.add(new Label(a.getID1()+","+a.getID2()),qtdeInc,0);
+        gpmatrizinc.add(new Label(""+1),qtdeInc,a.getID1()-64);
+        gpmatrizinc.add(new Label(""+1),qtdeInc++,a.getID2()-64);
     }
     
     private void atualizaMatriz(char id){
-        //g.getVlist().size();
-        gpmatrizadj.add(new Label(""+id),qtdeV,0);
-        gpmatrizadj.add(new Label(""+id),0,qtdeV);
         
-        gpmatrizinc.add(new Label(""+id),0,qtdeV);
+        gpmatrizadj.add(new Label(""+id),g.getVlist().size(),0);
+        gpmatrizadj.add(new Label(""+id),0,g.getVlist().size());
+        
+        for (int i = 1; i <= g.getVlist().size(); i++)
+            gpmatrizadj.add(new Label(""+0),g.getVlist().size(),i);
+        for (int i = 1; i <= g.getVlist().size(); i++)
+            gpmatrizadj.add(new Label(""+0),i,g.getVlist().size());
+        
+        gpmatrizinc.add(new Label(""+id),0,g.getVlist().size());
     }
 
     @FXML
@@ -130,12 +167,11 @@ public class TelaPrincipalController implements Initializable {
             if(vv.getDist().getBoundsInParent().intersects(v.getAp().getBoundsInParent()))
                 vdd = false;
         }
-        if(vdd && qtdeV < 10)
+        if(vdd && g.getVlist().size() < 10)
         {
             g.getVlist().add(v);
             mouseEvents(v);
             v.setID(cont);
-            qtdeV++;
             atualizaMatriz(cont);
         }
         else
